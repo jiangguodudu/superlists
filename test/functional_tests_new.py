c5c4ca31c5f3e024a2af7376ecac559cfd5719ff
jiangguodudu <274662093@qq.com>
2020-08-15 12:39:06 +0800

    commit functional_tests_new

--- test/functional_tests_new.py
+++ test/functional_tests_new.py
@@ -5,7 +5,10 @@
 # @Date  : 2020/8/15 0015
 # @Desc  :
 import unittest
+
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVistorTest(unittest.TestCase):
@@ -22,9 +25,32 @@ class NewVistorTest(unittest.TestCase):
 
         # 网页标题和头部都应包含"To-Do"
         self.assertIn("To-Do", self.driver.title)
-        self.fail("finish the test!")
+
+        header_text = self.driver.find_element_by_tag_name("h1").text
+        self.assertIn('To-Do', header_text)
 
         # 输入一个代办事项
+        input_box = self.driver.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 输入"Buy a peacock feathers"
+        input_box.send_keys("Buy a peacock feathers")
+
+        # 按下回车键，页面更新
+        # 待办事项列表显示“1: Buy a peacock feathers”
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        table = self.driver.find_element_by_id("id_list_table")
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy a peacock feathers' for row in rows)
+        )
+
+        self.fail("finish the test!")
 
 
 if __name__ == '__main__':
