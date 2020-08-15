c629968d510c6c2dbfd6a20d294c467536a75b35
jiangguodudu <274662093@qq.com>
2020-08-15 13:04:00 +0800

    change views tests home.html and workspace

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -16,6 +17,18 @@ class SmokeTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith("</html>"))
+
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        html = response.content.decode("utf-8")
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith("</html>"))
+        self.assertTemplateUsed(response, "home.html")
+
