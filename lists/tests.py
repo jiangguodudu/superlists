f8f3d9468780c3cb68ba6f99b6b707ca888d25dc
jiangguodudu <274662093@qq.com>
2020-08-22 22:40:37 +0800

    commit lists new

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -100,4 +100,35 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={"item_text": 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+
+class ListAndItemModel(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = "The first list item"
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "the second item"
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, "The first list item")
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "the second item")
+        self.assertEqual(second_saved_item.list, list_)
+
 
